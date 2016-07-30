2e0ac37ae97cab67ab5299919f3bea0ff2d5b7c7
fmaraque <fmaraque@emergya.com>
2016-07-30 11:41:55 +0200

    First upload

---
+++ src/main/java/com/javacodegeeks/testng/maven/testngMaven/App.java
@@ -0,0 +1,13 @@
+package com.javacodegeeks.testng.maven.testngMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/javacodegeeks/testng/maven/testngMaven/TestNgMavenExampleTest.java
@@ -0,0 +1,17 @@
+package com.javacodegeeks.testng.maven.testngMaven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNgMavenExampleTest {
+	
+	@Test
+	public void exampleOfTestNgMaven() {
+		System.out.println("This is TestNG-Maven Example");
+	}
+	
+	@Test
+	public void areTheNumbersEquals() {
+		Assert.assertEquals(3, 2,"Number aren't the same");
+	}
+}
