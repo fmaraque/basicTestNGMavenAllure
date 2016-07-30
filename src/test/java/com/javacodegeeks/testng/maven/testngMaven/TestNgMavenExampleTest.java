9870bb40808deae25dba00cccfae6d08a51ba302
fmaraque <fmaraque@emergya.com>
2016-07-30 12:41:56 +0200

    New Test added

--- src/test/java/com/javacodegeeks/testng/maven/testngMaven/TestNgMavenExampleTest.java
+++ src/test/java/com/javacodegeeks/testng/maven/testngMaven/TestNgMavenExampleTest.java
@@ -12,6 +12,6 @@ public class TestNgMavenExampleTest {
 	
 	@Test
 	public void areTheNumbersEquals() {
-		Assert.assertEquals(3, 3,"Number aren't the same");
+		Assert.assertEquals(4, 4,"Number aren't the same");
 	}
 }
